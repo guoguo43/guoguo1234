8f8e5d52dc4f7255b51632b596ec0837c78ab7e8
yaozeyuan <yaozeyuan001@lianjia.com>
2019-05-18 15:27:44 +0800

    fix: 漏了一个括号

--- my_google.py
+++ my_google.py
@@ -31,9 +31,9 @@ csvfile_names = os.listdir('.\\URL_GOOGLE_CSV')
 txtfile = open('google_results.txt', 'a+', encoding='utf-8')
 logfile = open('runtime_log.txt', 'a+', encoding='utf-8')
 #   简易log函数
-def log(*logContentList):
-    content = '{}:'.format(time.strftime('%Y-%m-%d %H:%M:%S')
-    for logItem in logContentList:
+def log(*logItemList):
+    content = '{}:'.format(time.strftime('%Y-%m-%d %H:%M:%S'))
+    for logItem in logItemList:
         content = '{}{}'.format(content, logItem)
     logfile.write(content + '\n')
     print(content)
